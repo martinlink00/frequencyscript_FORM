fee9293e73a8ef5b33f9741c0c78015e9a69599b
martinlink00 <69633819+martinlink00@users.noreply.github.com>
2022-12-13 12:01:10 +0100

    bash 2345 added

---
+++ run2345.bash
@@ -0,0 +1,32 @@
+{
+
+rm params.txt
+echo "5" > params.txt
+math12 -script freq.wls
+rm params.txt
+echo "4" > params.txt
+math12 -script freq.wls
+rm params.txt
+echo "3" > params.txt
+math12 -script freq.wls
+rm params.txt
+echo "2" > params.txt
+math12 -script freq.wls
+
+# Try
+} || {
+# Executed when above fails
+rm params.txt
+echo "5" > params.txt
+math -script freq.wls
+rm params.txt
+echo "4" > params.txt
+math -script freq.wls
+rm params.txt
+echo "3" > params.txt
+math -script freq.wls
+rm params.txt
+echo "2" > params.txt
+math -script freq.wls
+
+}
